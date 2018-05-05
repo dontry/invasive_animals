a66df42c7812cb5635e2b7187f3996fa6c3aa0eb
dontry <mccoy018@gmail.com>
2018-05-05 23:40:32 +1000

    created ConfirmationDialog

---
+++ src/components/common/ConfirmationDialog.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Dialog, { DialogActions, DialogContent } from "material-ui/Dialog";
+import { grey } from "material-ui/colors";
+import { Title, Paragraph } from "../common/Text";
+import { StyledButton } from "../common/ActionButtonGroup";
+
+const ConfirmationDialog = ({ handleClose, title, message, ...rest }) => {
+  return (
+    <Dialog
+      disableBackdropClick
+      disableEscapeKeyDown
+      maxWidth="xs"
+      aria-lablledby={title}
+      {...rest}
+      style={{padding: '2rem'}}
+    >
+      <DialogContent>
+        <Paragraph variant="title" txtColor={grey[700]}>
+          {message}
+        </Paragraph>
+      </DialogContent>
+      <DialogActions>
+        <StyledButton
+          onClick={handleClose}
+          type="message"
+          trait="main"
+          variant="flat"
+        >
+          Confirm
+        </StyledButton>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ConfirmationDialog;
